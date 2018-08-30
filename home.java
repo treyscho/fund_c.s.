37ab7387de822bf67a1636c2ffa77e7eea7de33d
treyscho <30322142+treyscho@users.noreply.github.com>
2018-08-30 14:52:16 -0400

    Add files via upload

--- home.java
+++ home.java
@@ -8,8 +8,8 @@
 public class home
 {
     public static void main(String[] args){
-        System.out.println("  \/^^^^^^^^^^^\\");
-        System.out.println(" \/            \\");
+        System.out.println("  /^^^^^^^^^^^\\");
+        System.out.println(" /            \\");
         System.out.println("|          _ _  |");
         System.out.println("|         |_|_| |");
         System.out.println("|         |_|_| |");
