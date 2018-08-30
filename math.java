65e363791fe8a3da14dfbe4214f5052d0b5c2cf7
treyscho <30322142+treyscho@users.noreply.github.com>
2018-08-30 15:28:30 -0400

    Add files via upload

--- math.java
+++ math.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 /**
  * Write a description of class math here.
  * 
@@ -7,27 +7,13 @@
  */
 public class math
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class math
-     */
-    public math()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public static void main(String[] args){
+        int f = 5;
+        int s = 5;
+        System.out.println("the sum of " + f + " " + s + " " + "is" + (f + s));
+        System.out.println("the difference of " + f + " " + s  + " " + "is" + (f - s));
+        System.out.println("the product of " + f + " " + s+ " " +"is" + (f * s));
+        System.out.println("the dividend of " + f + " " + s + " " + "is" + (f / s));
+        
     }
 }
